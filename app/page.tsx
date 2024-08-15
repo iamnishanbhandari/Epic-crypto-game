7938d2b13b8c7f311d0e3e96e36daca4859d098b
BBK010 <bibekkunwar238@gmail.com>
2024-08-15 20:29:19 +0545

    Bug Solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Layout from "./layout";
 import GameCard from "./components/GameCard";
 
 const HomePage = () => {
@@ -12,18 +11,14 @@ const HomePage = () => {
   ];
 
   return (
-    <Layout>
-      <div className="flex flex-col items-center justify-start min-h-screen pt-8">
-        <h1 className="text-center text-3xl font-bold mb-8">
-          Epic Crypto Games
-        </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {games.map((game, index) => (
-            <GameCard key={index} imageSrc={game.src} altText={game.alt} />
-          ))}
-        </div>
+    <div className="flex flex-col items-center justify-start min-h-screen pt-8">
+      <h1 className="text-center text-3xl font-bold mb-8">Epic Crypto Games</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {games.map((game, index) => (
+          <GameCard key={index} imageSrc={game.src} altText={game.alt} />
+        ))}
       </div>
-    </Layout>
+    </div>
   );
 };
 
