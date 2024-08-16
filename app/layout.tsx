dea89342d9611837fa12dc622c21f89ae420aa41
BBK010 <bibekkunwar238@gmail.com>
2024-08-16 12:25:24 +0545

    Update RootLayout to fix crossOrigin issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,8 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
           rel="stylesheet"
